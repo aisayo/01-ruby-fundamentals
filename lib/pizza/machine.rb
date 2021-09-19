c21c8793a0ab5013205ebb2dd14667e8c3a72518
Aysan Isayo <aisayo@icloud.com>
2021-09-19 14:12:07 -0700

    first commit

---
+++ lib/pizza/machine.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "machine/version"
+
+module Pizza
+  module Machine
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/pizza/machine/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Pizza
+  module Machine
+    VERSION = "0.1.0"
+  end
+end
